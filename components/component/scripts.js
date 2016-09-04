8d91dd24c8fae24892fbefe0373a73b1f32599f4
Vincent Smuda <vincent@vincentsmuda.com>
2016-09-04 16:10:58 -0400

    adjusted modifiers to be less repetative by abstracting the '--'

--- components/component/scripts.js
+++ components/component/scripts.js
@@ -1,5 +1,12 @@
+/*
+
+	COMPONENT component
+	This is where the logic goes for the generic component.
+
+*/
+
 (function(){
 	
-	var component = document.getElementsByClassName('component-COMPONENT');
+	var component = document.getElementsByClassName('COMPONENT');
 
 })();
